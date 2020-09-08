7dfb8333ad85d63921c7a951c647b2543169d7dc
Mark Butcher <markjamesbutcher@gmail.com>
2020-09-08 21:19:06 +1000

    listed items

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Text, Button, FlatList } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Push the button');
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = text => {
+    setEnteredGoal(text)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ]);
+  };
 
   return (
     <View style={{ padding: 40 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput style={{ height: 45, width: "80%", fontSize: 20, borderBottomColor: "black", borderBottomWidth: 1, marginBottom: 20, textAlign: "center" }}
-          placeholder="Course Goal" />
-        <Button title="ADD" />
+      <View style={styles.inputContainerStye}>
+        <TextInput style={styles.inputStyle}
+          placeholder="Course Goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-
-    </View>
+      <FlatList
+        data={courseGoals}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => item.id}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text style={styles.text}>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+    </View >
   );
 }
 
@@ -24,4 +48,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainerStye: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputStyle: {
+    height: 45,
+    width: "80%",
+    fontSize: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    textAlign: "center"
+  },
+  listItem: {
+    padding: 4,
+    marginVertical: 10,
+    backgroundColor: '#fcf',
+  },
+  text: {
+    fontSize: 22
+  }
 });
