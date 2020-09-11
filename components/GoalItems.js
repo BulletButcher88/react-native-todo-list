2c825a8fab5fcb0f488ae91f3d2fc026c7feb2a5
Mark Butcher <markjamesbutcher@gmail.com>
2020-09-11 15:42:03 +1000

    randomise goal backgroundColor

--- components/GoalItems.js
+++ components/GoalItems.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
+const colorArrayRan = ['a', 'b', 'c', 'd', 'f']
+const randomNum = () => {
+  return Math.floor(Math.random() * 4)
+}
+const randomColor = () => {
+  return `#${colorArrayRan[randomNum()]}c${colorArrayRan[randomNum()]}`
+}
 
 const GoalItems = props => {
   return (
     <TouchableOpacity activeOpacity={0.5} onPress={props.onDelete.bind(this, props.id)}>
-      <View style={styles.listItem}>
+      <View style={{
+        backgroundColor: randomColor(), borderRadius: 10,
+        padding: 4,
+        marginVertical: 10,
+      }}>
         <Text style={styles.text}>{props.title}</Text>
       </View>
-    </TouchableOpacity>
+    </TouchableOpacity >
   )
 }
 
 const styles = StyleSheet.create({
-  listItem: {
-    borderRadius: 10,
-    padding: 4,
-    marginVertical: 10,
-    backgroundColor: '#fcf',
-  },
   text: {
     fontSize: 22
   }
